554ec485691410c6909aaf4f67dd10fd80eeede3
Tinnikx <kaixiang.tao@egym.com>
2021-08-26 15:21:44 +0800

    update

--- user/src/main/java/com/kaixiang/module/user/UserApplication.java
+++ user/src/main/java/com/kaixiang/module/user/UserApplication.java
@@ -6,10 +6,12 @@ import com.kaixiang.security.configuration.EnableSecurity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableSecurity
 @EnableCommonLib
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.kaixiang.module.user.repository")
 public class UserApplication {
 
